0b04fad77e4c57979d1844f1f7a9c4032a86c681
Joseph Cancellieri <112720941+jcancell@users.noreply.github.com>
2023-03-21 16:48:59 -0400

    Update sum_even_fibonacci.cc

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -4,7 +4,9 @@ int main()
 {
     int a = 0, b = 1;
     int sum_even = 0;
+    // fibonacci sequence stops at 4 million
     while (b < 4000000) {
+        // if the current number is even, add it to the sum_even
         if (b % 2 == 0) {
             sum_even += b;
         }
