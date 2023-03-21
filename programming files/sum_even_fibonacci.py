686172f562bc08cdeddd20ffa2c6f6940cac5e87
dlaberg2 <112562809+dlaberg2@users.noreply.github.com>
2023-03-21 16:53:42 -0400

    Update sum_even_fibonacci.py
    Introduced 4 bugs into the program

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,12 +1,13 @@
+# Bugs Introduced: DML
 a, b = 0, 1
 sum_even = 0
-while b < 4000000:
+while b > 4000000:
     # if the number is even, add it to sum_even
-    if b % 2 == 0:
+    if b % 2 == 1:
         sum_even += b
     # set a to b, and set b to a + b
-    a, b = b, a+b
+    a, b = b, a-b
 
-print(sum_even)
+print(sum_even
 
 # answer should be 4613732
