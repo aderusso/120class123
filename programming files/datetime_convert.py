00328a77d6e80b75afde144f4cc1c8c56cdd6206
dlaberg2 <112562809+dlaberg2@users.noreply.github.com>
2023-03-21 16:58:04 -0400

    Update datetime_convert.py
    Added Comments and bugs

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -1,7 +1,8 @@
-from datetime import datetime
-
+# Bugs Introduced: DML
+from datetime import
+# Formats the date
 date_str = "2022-03-17 10:45:30"
 date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 formatted_date = date_obj.strftime('%m/%d/%Y %H:%M:%S')
-
-print(formatted_date)
+# Print the date
+print(formatted_date
