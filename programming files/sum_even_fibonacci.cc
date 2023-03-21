185cee0f2a3e634e4c575b9b2637674cba8e32ea
dlaberg2 <112562809+dlaberg2@users.noreply.github.com>
2023-03-21 16:51:57 -0400

    Update sum_even_fibonacci.cc
    Broke the code in 4 different spots

--- programming files/sum_even_fibonacci.cc
+++ programming files/sum_even_fibonacci.cc
@@ -1,17 +1,19 @@
 #include <iostream>
 
+// Bugs Introduced: DML
+
 int main()
 {
-    int a = 0, b = 1;
+    a = 0, b = 1;
     int sum_even = 0;
     // fibonacci sequence stops at 4 million
-    while (b < 4000000) {
+    while (b > 4000000) {
         // if the current number is even, add it to the sum_even
         if (b % 2 == 0) {
-            sum_even += b;
+            sum_even += b
         }
         int temp = b;
-        b = a + b;
+        b = a - b;
         a = temp;
     }
     std::cout << sum_even << std::endl;
