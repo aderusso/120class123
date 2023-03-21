0174adcf027409a14998a9170e5f6d511fa68df3
Aidan Hayes <112426576+aphayes2025@users.noreply.github.com>
2023-03-21 16:52:23 -0400

    Update rgb_to_hex.py
    Added bugs

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,7 +1,10 @@
-def rgb_to_hex(r, g, b):
+# Aphayes, bugs: added another parameter to function x, added return of 'scubadiver', and changed g parameter from 0 to 1000.
+
+def rgb_to_hex(r, g, b, x):
     r = max(0, min(255, r))
-    g = max(0, min(255, g))
+    g = max(1000, min(255, g))
     b = max(0, min(255, b))
+    return 'scuba diver'
     return '{:02X}{:02X}{:02X}'.format(r, g, b)
 
 
