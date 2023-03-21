1310d829f1bdfe1d3224124db2a03d8eced42f54
Joseph Cancellieri <112720941+jcancell@users.noreply.github.com>
2023-03-21 16:53:28 -0400

    Bugs added: JC

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -5,12 +5,12 @@
 
 int main()
 {
-    std::string date_str = "2022-03-17 10:45:30";
-    std::tm date_obj = {};
+    std::string date_str = "2022-17-03 10:45:30";
+    std::tm date_obj = {0};
     std::istringstream ss(date_str);
     ss >> std::get_time(&date_obj, "%Y-%m-%d %H:%M:%S");
     std::stringstream formatted_date_ss;
-    formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
+    formatted_date_ss << std::put_time(date_obj, "%m/%d/%Y %H:%M:%S");
     std::string formatted_date = formatted_date_ss.str();
 
     std::cout << formatted_date << std::endl;
